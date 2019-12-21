444a6df2a616129aba5f6b28e1f8c6befb925cb6
Gaston Bengolea <gbengolea@google.com>
2019-12-21 00:59:05 +0000

    Je m'apelle Claude

--- french2ipa/script.js
+++ french2ipa/script.js
@@ -1,13 +1,49 @@
 function updateTranslation() {
-    console.log('updateTranslation');
-    document.getElementById("result").innerText = translation(
-        document.getElementById("phrase").value);
-}
-
-function translation(s) {
-    return s.toUpperCase();
+    document.getElementById("result").innerText =
+        translation(document.getElementById("phrase").value);
 }
 
 addLoadEvent(function () {
     updateTranslation();
 });
+
+function translation(s) {
+    s = s.toLowerCase();
+    let result = '';
+    for (let i = 0; i < s.length; i++) {
+        console.log(i, s[i], i < s.length - 1, handle2Letters(s[i], s[i+1]) );
+        if (i < s.length - 1 && handle2Letters(s[i], s[i+1])) {
+            console.log('handle ${i}');
+            result += translate2Letters(s[i], s[i+1]);
+            i++;
+            continue; 
+        }
+        result += translateLetter(s[i]);
+    }
+    return result;
+}
+
+const constant = "abdfiklmnop ";
+const others = {
+    "j": "ʒ",
+    "e": "ə",
+    "c": "k",
+    "'": "",
+    ".": "",
+}
+
+// Order matters.
+const twoLetters = ["au", "je", "e ", "e.", "ll"];
+const twoLettersTrad = ["o", "ʒə", " ", " ", "l"];
+
+handle2Letters = (a, b) => twoLetters.indexOf(`${a}${b}`) != -1;
+
+translate2Letters = (a, b) => twoLettersTrad[twoLetters.indexOf(`${a}${b}`)];
+
+function translateLetter(c) {
+    if (constant.indexOf(c) != -1)
+        return c;
+    if (c in others)
+        return others[c];
+    return "?";
+}
